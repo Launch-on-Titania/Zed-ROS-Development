f75bb2870110b3894b67aae31d00410324b25e2d
Launch-on-Titania <arnoxtmann@gmail.com>
2024-03-11 12:08:04 +0800

    updates record parameters

--- src/rosbag_process.sh
+++ src/rosbag_process.sh
@@ -5,5 +5,5 @@ date=$1
 echo "Creat folder"
 mkdir /home/arno/Projects/zed_ws/outputs/records/zed_${date}
 
-sleep 7
-rosbag record -o /home/arno/Projects/zed_ws/outputs/records/zed_${date}/zed_${date}.bag /zed/zed_node/rgb/camera_info /zed/zed_node/depth/camera_info /zed/zed_node/rgb/image_rect_color /zed/zed_node/depth/depth_registered -b 0
+sleep 10
+rosbag record -o /home/arno/Projects/zed_ws/outputs/records/zed_${date}/zed_${date}.bag /zed/zed_node/rgb/camera_info /zed/zed_node/depth/camera_info /zed/zed_node/rgb/image_rect_color /zed/zed_node/depth/depth_registered -b 0 --duration=30
